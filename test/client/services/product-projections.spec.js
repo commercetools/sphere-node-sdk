7e695c54dfd51747c461782ce5c0e4411962aa99
Nicola Molinari <nicola.molinari@commercetools.de>
2015-04-22 22:25:43 +0200

    Really first draft of object composition

--- test/client/services/product-projections.spec.js
+++ test/client/services/product-projections.spec.js
@@ -3,6 +3,13 @@ import { productProjections } from '../../../lib/client/services'
 describe('ProductProjections', () => {
 
   it('should initialize client', () => {
+    expect(productProjections.baseEndpoint).toBe('/product-projections')
+    expect(productProjections.byId).toEqual(jasmine.any(Function))
+    expect(productProjections.where).toEqual(jasmine.any(Function))
+    expect(productProjections.fetch).toEqual(jasmine.any(Function))
+  })
+
+  it('should build fetch url', () => {
     const requestStub = jasmine.createSpy('request')
     const opts = {
       request: {
