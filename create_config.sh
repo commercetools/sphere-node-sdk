6e2f3c324e1fe8738317124dd26975d447cb587b
Nicola Molinari <nicola.molinari@commercetools.de>
2015-05-03 23:11:51 +0200

    First draft of auth implementation

--- create_config.sh
+++ create_config.sh
@@ -2,7 +2,7 @@
 
 cat > "config.js" << EOF
 /* SPHERE.IO credentials */
-exports.config = {
+module.exports = {
   client_id: "${SPHERE_CLIENT_ID}",
   client_secret: "${SPHERE_CLIENT_SECRET}",
   project_key: "${SPHERE_PROJECT_KEY}"
---
+++ test/integration/auth.spec.js
@@ -0,0 +1,38 @@
+import * as auth from '../../lib/utils/auth'
+import http from '../../lib/utils/http'
+import credentials from '../../../config'
+
+const authRequest = auth.buildRequest({
+  projectKey: credentials.project_key,
+  clientId: credentials.client_id,
+  clientSecret: credentials.client_secret
+})
+
+describe('Integration - Auth', () => {
+
+  let httpFetch
+
+  beforeEach(() => {
+    httpFetch = http({
+      Promise: Promise,
+      request: {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': `${authRequest.body.length}`
+        },
+        timeout: 20000
+      }
+    })
+  })
+
+  it('should request a new token', done => {
+    httpFetch.post(authRequest.endpoint, authRequest.body)
+      .then(res => res.json())
+      .then(res => {
+        expect(res.access_token).toEqual(jasmine.any(String))
+        done()
+      })
+      .catch(done.fail)
+  })
+
+})
