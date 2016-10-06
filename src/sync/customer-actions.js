37f5d76ec35c9ab05b93bccdfbdfce9e5f415c18
Joshua Bones <joshua.bones@commercetools.de>
2016-10-06 16:44:02 +0200

    feat(customerAddresses): implement customer address action building

--- src/sync/customer-actions.js
+++ src/sync/customer-actions.js
@@ -2,6 +2,11 @@ import {
   buildBaseAttributesActions,
   buildReferenceActions,
 } from './utils/common-actions'
+import createBuildNestedObjectActions, {
+  CREATE_ACTIONS,
+  REMOVE_ACTIONS,
+  CHANGE_ACTIONS,
+} from './utils/create-build-nested-object-actions'
 
 export const baseActionsList = [
   { action: 'changeEmail', key: 'email' },
@@ -42,7 +47,34 @@ export function actionsMapReferences (diff, oldObj, newObj) {
   })
 }
 
-export function actionsMapAddresses (/* diff, oldObj, newObj */) {
-  const actions = []
-  return actions
+export function actionsMapAddresses (diff, oldObj, newObj) {
+  function addAddressActionBuilder (oldArray, newArray, index) {
+    return {
+      action: 'addAddress',
+      address: newArray[index],
+    }
+  }
+
+  function removeAddressActionBuilder (oldArray, newArray, index) {
+    return {
+      action: 'removeAddress',
+      addressId: oldArray[index].id,
+    }
+  }
+
+  function changeAddressActionBuilder (oldArray, newArray, index) {
+    return {
+      action: 'changeAddress',
+      addressId: oldArray[index].id,
+      address: newArray[index],
+    }
+  }
+
+  const handler = createBuildNestedObjectActions('addresses', {
+    [CREATE_ACTIONS]: addAddressActionBuilder,
+    [REMOVE_ACTIONS]: removeAddressActionBuilder,
+    [CHANGE_ACTIONS]: changeAddressActionBuilder,
+  })
+
+  return handler(diff, oldObj, newObj)
 }
