a9c69e4538cf2bade35b628349bcc3c0f9cf234b
Joshua Bones <joshua.bones@commercetools.de>
2016-10-06 16:44:32 +0200

    refactor(productActions): refactor variant action builder to use nested object builder utility

--- src/sync/product-actions.js
+++ src/sync/product-actions.js
@@ -6,6 +6,10 @@ import {
   buildBaseAttributesActions,
   buildReferenceActions,
 } from './utils/common-actions'
+import createBuildNestedObjectActions, {
+  CREATE_ACTIONS,
+  REMOVE_ACTIONS,
+} from './utils/create-build-nested-object-actions'
 
 const REGEX_NUMBER = new RegExp(/^\d+$/)
 const REGEX_UNDERSCORE_NUMBER = new RegExp(/^_\d+$/)
@@ -51,26 +55,23 @@ export function actionsMapMeta (diff, oldObj, newObj) {
 }
 
 export function actionsMapVariants (diff, oldObj, newObj) {
-  const actions = []
-  if (!diff.variants) return actions
+  function addVariantActionBuilder (oldArray, newArray, index) {
+    return Object.assign(newArray[index], { action: 'addVariant' })
+  }
 
-  const addVariantActions = []
-  const removeVariantActions = []
-  forEach(diff.variants, (variant, index) => {
-    if (REGEX_NUMBER.test(index) && Array.isArray(variant)) {
-      const newVariant = newObj.variants[index]
-      const action = Object.assign({ action: 'addVariant' }, newVariant)
-      addVariantActions.push(action)
-    } else if (REGEX_UNDERSCORE_NUMBER.test(index) && Array.isArray(variant))
-      // If array move do nothing, otherwise remove the variant
-      if (!(variant.length === 3 && variant[2] === 3))
-        removeVariantActions.push({
-          action: 'removeVariant', id: variant[0].id,
-        })
+  function removeVariantActionBuilder (oldArray, newArray, index) {
+    return {
+      action: 'removeVariant',
+      id: oldArray[index].id,
+    }
+  }
+
+  const handler = createBuildNestedObjectActions('variants', {
+    [CREATE_ACTIONS]: addVariantActionBuilder,
+    [REMOVE_ACTIONS]: removeVariantActionBuilder,
   })
 
-  // Make sure `removeVariant` actions come first
-  return removeVariantActions.concat(addVariantActions)
+  return handler(diff, oldObj, newObj)
 }
 
 export function actionsMapReferences (diff, oldObj, newObj) {
